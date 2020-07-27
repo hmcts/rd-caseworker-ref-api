95a6f25f2588d30cb0510220f80b608cb3998eb8
Nitish Singla <nitish.singla@hmcts.net>
2020-07-27 15:43:07 +0100

    RD: Initial checking for RD Caseworker Ref API.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,41 @@
+# Temporary fix for template API version error on deployment
 provider "azurerm" {
-  features {}
+  version = "1.22.0"
+}
+
+locals {
+  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
+  preview_app_service_plan = "${var.product}-${var.component}-${var.env}"
+  non_preview_app_service_plan = "${var.product}-${var.env}"
+  app_service_plan = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
+
+  preview_vault_name = "${var.raw_product}-aat"
+  non_preview_vault_name = "${var.raw_product}-${var.env}"
+  key_vault_name = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
+
+}
+
+data "azurerm_key_vault" "rd_key_vault" {
+  name = "${local.key_vault_name}"
+  resource_group_name = "${local.key_vault_name}"
+}
+
+data "azurerm_key_vault_secret" "ACCOUNT_NAME" {
+  name = "ACCOUNT-NAME"
+  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+}
+
+data "azurerm_key_vault_secret" "ACCOUNT_KEY" {
+  name = "ACCOUNT-KEY"
+  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+}
+
+data "azurerm_key_vault_secret" "CONTAINER_NAME" {
+  name = "CONTAINER-NAME"
+  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+}
+
+data "azurerm_key_vault_secret" "BLOB_URL_SUFFIX" {
+  name = "BLOB-URL-SUFFIX"
+  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,17 +1,64 @@
-variable "product" {}
+variable "product" {
+  type = "string"
+}
 
-variable "component" {}
+variable "raw_product" {
+  default = "rd" // jenkins-library overrides product for PRs and adds e.g. pr-123-ia
+}
+
+variable "component" {
+  type = "string"
+}
 
 variable "location" {
+  type    = "string"
   default = "UK South"
 }
 
-variable "env" {}
+variable "env" {
+  type = "string"
+}
 
-variable "subscription" {}
+variable "subscription" {
+  type = "string"
+}
 
-variable "deployment_namespace" {}
+variable "ilbIp" {}
 
 variable "common_tags" {
   type = "map"
 }
+
+variable "capacity" {
+  default = "1"
+}
+
+variable "instance_size" {
+  default = "I1"
+}
+
+variable "appinsights_instrumentation_key" {
+  default = ""
+}
+
+variable "root_logging_level" {
+  default = "INFO"
+}
+
+variable "log_level_spring_web" {
+  default = "INFO"
+}
+
+variable "log_level_rd" {
+  default = "INFO"
+}
+
+variable "team_name" {
+  default = "RD"
+}
+
+variable "managed_identity_object_id" {
+  default = ""
+}
+
+
