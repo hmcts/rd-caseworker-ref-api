5bd4610a4b0f76f4963b5d0ef4778f5873181a9e
AdilOoze <adil.oozeerally@hmcts.net>
2020-11-03 17:20:50 +0000

    RDCC-1979: adding component name in log

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/servicebus/messaging/JmsErrorHandler.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/servicebus/messaging/JmsErrorHandler.java
@@ -1,15 +1,19 @@
 package uk.gov.hmcts.reform.cwrdapi.servicebus.messaging;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.NonNull;
 import org.springframework.util.ErrorHandler;
 
 @Slf4j
 public class JmsErrorHandler implements ErrorHandler {
 
+    @Value("${loggingComponentName}")
+    private String loggingComponentName;
+
     @Override
     public void handleError(@NonNull Throwable throwable) {
-        log.warn("Spring JMS custom error handling example");
+        log.warn("{}:: Spring JMS custom error handling example", loggingComponentName);
         log.error(throwable.getCause().getMessage());
     }
 }
