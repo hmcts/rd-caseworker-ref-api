517b83ff0cfd1a891b1ab844ee736b47c92ae406
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2020-11-06 11:07:39 +0000

    feat:adding integration test case to create caseworker profile

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/cwrdapi/CreateCaseWorkerProfileIntTest.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.reform.cwrdapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import uk.gov.hmcts.reform.cwrdapi.controllers.request.CaseWorkerProfileCreationRequest;
+import uk.gov.hmcts.reform.cwrdapi.util.AuthorizationEnabledIntegrationTest;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class CreateCaseWorkerProfileIntTest extends AuthorizationEnabledIntegrationTest {
+
+    @Test
+    public void persists_and_returns_valid_organisation_with_contact_and_dxAddress() {
+        CaseWorkerProfileCreationRequest creationRequest = new CaseWorkerProfileCreationRequest();
+        Map<String, Object> response = caseworkerReferenceDataClient
+                .createCaseWorkerProfile(creationRequest, cwrdAdmin);
+        assertThat(response);
+
+    }
+
+
+
+}
---
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/request/CaseWorkerProfileCreationRequest.java
@@ -0,0 +1,17 @@
+package uk.gov.hmcts.reform.cwrdapi.controllers.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder(builderMethodName = "anOrganisationCreationRequest")
+public class CaseWorkerProfileCreationRequest {
+
+    public CaseWorkerProfileCreationRequest() {
+
+    }
+
+
+}
