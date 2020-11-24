b68ac1c3d02d4b4bf2e45901b4bd2e52941e5537
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-11-24 15:56:53 +0000

    RDCC-1952 removed unwanted code

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/CaseWorkerRefApiApplication.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/CaseWorkerRefApiApplication.java
@@ -1,9 +1,8 @@
 package uk.gov.hmcts.reform.cwrdapi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,10 +11,12 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 @EnableJpaAuditing
 @EnableJpaRepositories
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {MultipartAutoConfiguration.class})
-@EnableFeignClients(basePackages = {"uk.gov.hmcts.reform.cwrdapi" }, basePackageClasses = { IdamApi.class }
+@EnableCircuitBreaker
+@EnableFeignClients(basePackages = {
+    "uk.gov.hmcts.reform.cwrdapi" },
+    basePackageClasses = { IdamApi.class }
 )
-@SuppressWarnings("HideUtilityClassConstructor")
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class CaseWorkerRefApiApplication {
 
     public static void main(final String[] args) {
--- src/main/java/uk/gov/hmcts/reform/cwrdapi/service/ExcelAdaptorService.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/service/ExcelAdaptorService.java
@@ -1,4 +1,9 @@
 package uk.gov.hmcts.reform.cwrdapi.service;
 
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.util.List;
+
 public interface ExcelAdaptorService {
+    List<Object> parseExcel(Workbook workbook, Class classType);
 }
