933d9837f909a04608f0f515d36d38a2d422c897
Nitish Singla <nitish.singla@hmcts.net>
2021-01-05 16:45:45 +0000

    RDCC-2077: Minor code refactoring.

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/request/UserRequest.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/request/UserRequest.java
@@ -2,15 +2,15 @@ package uk.gov.hmcts.reform.cwrdapi.controllers.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
 
-@Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class UserRequest implements Serializable {
     private List<String> userIds;
