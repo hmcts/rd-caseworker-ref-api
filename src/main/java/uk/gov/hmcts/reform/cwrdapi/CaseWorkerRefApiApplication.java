abd629ba9e965a62ded315507ceb53bc6a54395b
AdilOoze <adil.oozeerally@hmcts.net>
2021-01-11 12:21:03 +0000

    RDCC-2048: removing unused import

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/CaseWorkerRefApiApplication.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/CaseWorkerRefApiApplication.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.cwrdapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
