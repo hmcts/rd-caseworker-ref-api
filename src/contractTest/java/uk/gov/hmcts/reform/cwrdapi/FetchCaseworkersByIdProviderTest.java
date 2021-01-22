4d72d393ffc54107877361a3edb0c88ef9fc94e6
ConorH994 <conor.halpin@hmcts.net>
2021-01-22 18:18:30 +0000

    checkstyle

--- src/contractTest/java/uk/gov/hmcts/reform/cwrdapi/FetchCaseworkersByIdProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/cwrdapi/FetchCaseworkersByIdProviderTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
