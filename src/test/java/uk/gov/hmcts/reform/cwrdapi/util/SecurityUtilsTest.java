98230c4d3c1c35a166a4e1bc35a6274830fb56fc
Nitish Singla <nitish.singla@hmcts.net>
2021-01-23 08:44:34 +0000

    (feat) RDCC-2261: Fixing the SecurityUtilsTest.java

--- src/test/java/uk/gov/hmcts/reform/cwrdapi/util/SecurityUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/cwrdapi/util/SecurityUtilsTest.java
@@ -1,12 +1,13 @@
 package uk.gov.hmcts.reform.cwrdapi.util;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.mockito.Mock;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SecurityUtilsTest {
 
@@ -22,7 +23,8 @@ public class SecurityUtilsTest {
             + ".LH3aiNniHNMlTwuSdzgRic9sD_4inQv5oUqJ0kkRKVasS4RfhIz2tRdttf-sSMkUga1p1teOt2iCq4BQBDS7KA";
 
     @Test
-    void getServiceAuthorizationHeader() {
+    public void getServiceAuthorizationHeader() {
+        when(authTokenGenerator.generate()).thenReturn(serviceAuthorization);
         String result = securityUtils.getServiceAuthorizationHeader();
         assertNotNull(result);
         assertTrue(result.contains(serviceAuthorization));
