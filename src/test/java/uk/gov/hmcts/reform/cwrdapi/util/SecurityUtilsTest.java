652cc9ca2f26767154bb53b82d09e38c8e1582ff
Nitish Singla <nitish.singla@hmcts.net>
2021-01-23 08:54:06 +0000

    (feat) RDCC-2261: Fixing the SecurityUtilsTest.java

--- src/test/java/uk/gov/hmcts/reform/cwrdapi/util/SecurityUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/cwrdapi/util/SecurityUtilsTest.java
@@ -1,11 +1,10 @@
 package uk.gov.hmcts.reform.cwrdapi.util;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mock;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -18,15 +17,12 @@ public class SecurityUtilsTest {
             authTokenGenerator
     );
 
-    private final String serviceAuthorization = "Bearer eyJhbGciOiJIUzUxMiJ9"
-            + ".eyJzdWIiOiJjY2RfZ3ciLCJleHAiOjE1OTQ2ODQ5MTF9"
-            + ".LH3aiNniHNMlTwuSdzgRic9sD_4inQv5oUqJ0kkRKVasS4RfhIz2tRdttf-sSMkUga1p1teOt2iCq4BQBDS7KA";
-
     @Test
     public void getServiceAuthorizationHeader() {
+        String serviceAuthorization = "Bearer eyJhbGciOiA";
         when(authTokenGenerator.generate()).thenReturn(serviceAuthorization);
         String result = securityUtils.getServiceAuthorizationHeader();
-        assertNotNull(result);
-        assertTrue(result.contains(serviceAuthorization));
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result.contains(serviceAuthorization));
     }
 }
