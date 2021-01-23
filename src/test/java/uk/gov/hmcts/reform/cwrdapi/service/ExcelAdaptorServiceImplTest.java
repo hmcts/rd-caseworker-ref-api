30103509d302e43baba041d88470103f6cb98870
Nitish Singla <nitish.singla@hmcts.net>
2021-01-23 10:10:58 +0000

    (feat) RDCC-2215: Initial checkin for the validations.

--- src/test/java/uk/gov/hmcts/reform/cwrdapi/service/ExcelAdaptorServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/cwrdapi/service/ExcelAdaptorServiceImplTest.java
@@ -3,10 +3,12 @@ package uk.gov.hmcts.reform.cwrdapi.service;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 import uk.gov.hmcts.reform.cwrdapi.advice.ExcelValidationException;
 import uk.gov.hmcts.reform.cwrdapi.client.domain.CaseWorkerProfile;
 import uk.gov.hmcts.reform.cwrdapi.client.domain.ServiceRoleMapping;
@@ -17,6 +19,7 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.cwrdapi.util.CaseWorkerConstants.FILE_MISSING_HEADERS;
 import static uk.gov.hmcts.reform.cwrdapi.util.CaseWorkerConstants.FILE_NO_DATA_ERROR_MESSAGE;
 import static uk.gov.hmcts.reform.cwrdapi.util.CaseWorkerConstants.FILE_NO_VALID_SHEET_ERROR_MESSAGE;
 import static uk.gov.hmcts.reform.cwrdapi.util.CaseWorkerConstants.REQUIRED_ROLE_MAPPING_SHEET_NAME;
@@ -26,6 +29,14 @@ public class ExcelAdaptorServiceImplTest {
 
     @InjectMocks
     ExcelAdaptorServiceImpl excelAdaptorServiceImpl;
+
+    @Before
+    public void initialize() {
+        List<String> acceptableHeaders = List.of("FIRST NAME", "LAST NAME", "Official Email",
+                "Region", "User type");
+        ReflectionTestUtils.setField(excelAdaptorServiceImpl, "acceptableHeaders",
+                acceptableHeaders);
+    }
 
     @Test
     public void parseXlsxShouldReturnWorkbookObjectTest() throws IOException {
@@ -102,4 +113,32 @@ public class ExcelAdaptorServiceImplTest {
         assertThat(serviceRoleMapping.getSerivceId()).isEqualTo("BBA9");
 
     }
+
+    @Test
+    public void sendXlsxWithIncorrectHeaders() throws IOException {
+        Workbook workbook = WorkbookFactory
+                .create(new File("src/test/resources/CaseWorkerUserXlsWithInvalidHeaders.xls"));
+
+        Assertions.assertThatThrownBy(() -> excelAdaptorServiceImpl.parseExcel(workbook, CaseWorkerProfile.class))
+                .isExactlyInstanceOf(ExcelValidationException.class)
+                .hasMessage(FILE_MISSING_HEADERS);
+    }
+
+    /*@Test
+    public void sendXlsxWithMissingLocation() throws IOException {
+        Workbook workbook = WorkbookFactory
+                .create(new File("src/test/resources/xlsxWithData_No_Location.xlsx"));
+        Assertions.assertThatThrownBy(() -> excelAdaptorServiceImpl.parseExcel(workbook, CaseWorkerProfile.class))
+                .isExactlyInstanceOf(ExcelValidationException.class)
+                .hasMessage(FILE_MISSING_HEADERS);
+    }*/
+
+    /*@Test
+    public void sendXlsxWithMissingFirstName() throws IOException {
+        Workbook workbook = WorkbookFactory
+                .create(new File("src/test/resources/xlsxWithData_Missing_First_Name.xlsx"));
+        Assertions.assertThatThrownBy(() -> excelAdaptorServiceImpl.parseExcel(workbook, CaseWorkerProfile.class))
+                .isExactlyInstanceOf(ExcelValidationException.class)
+                .hasMessage(FILE_MISSING_HEADERS);
+    }*/
 }
