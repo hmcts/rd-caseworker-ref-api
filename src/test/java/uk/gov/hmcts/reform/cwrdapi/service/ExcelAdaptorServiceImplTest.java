8cdee26200aea73b2642c24d2b718c14c9a23dfc
Nitish Singla <nitish.singla@hmcts.net>
2021-01-29 16:44:33 +0000

    (feat)RDCC-2201: Code refactoring.

--- src/test/java/uk/gov/hmcts/reform/cwrdapi/service/ExcelAdaptorServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/cwrdapi/service/ExcelAdaptorServiceImplTest.java
@@ -103,22 +103,4 @@ public class ExcelAdaptorServiceImplTest {
                 .isExactlyInstanceOf(ExcelValidationException.class)
                 .hasMessage(CaseWorkerConstants.FILE_MISSING_HEADERS);
     }
-
-    /*@Test
-    public void sendXlsxWithMissingLocation() throws IOException {
-        Workbook workbook = WorkbookFactory
-                .create(new File("src/test/resources/xlsxWithData_No_Location.xlsx"));
-        Assertions.assertThatThrownBy(() -> excelAdaptorServiceImpl.parseExcel(workbook, CaseWorkerProfile.class))
-                .isExactlyInstanceOf(ExcelValidationException.class)
-                .hasMessage(FILE_MISSING_HEADERS);
-    }*/
-
-    /*@Test
-    public void sendXlsxWithMissingFirstName() throws IOException {
-        Workbook workbook = WorkbookFactory
-                .create(new File("src/test/resources/xlsxWithData_Missing_First_Name.xlsx"));
-        Assertions.assertThatThrownBy(() -> excelAdaptorServiceImpl.parseExcel(workbook, CaseWorkerProfile.class))
-                .isExactlyInstanceOf(ExcelValidationException.class)
-                .hasMessage(FILE_MISSING_HEADERS);
-    }*/
 }
