760792bcee8608b4b7fa463a1e1fde75e890756b
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2021-02-08 12:22:43 +0000

    RDCC-2371 - corrected integration db files

--- src/integrationTest/resources/db/testmigration/V1_1__init_tables.sql
+++ src/integrationTest/resources/db/testmigration/V1_1__init_tables.sql
@@ -1,5 +1,11 @@
-
 create schema if not exists dbrdcaseworker;
+
+create sequence CASE_WORKER_LOCATION_ID_SEQ;
+create sequence CASE_WORKER_WORK_AREA_ID_SEQ;
+create sequence CASE_WORKER_ROLE_ID_SEQ;
+create sequence CW_IDAM_ROLE_ASSOC_ID_SEQ;
+create sequence JOB_ID_SEQ;
+create sequence EXCEPTION_ID_SEQ;
 
 create table case_worker_profile(
 	case_worker_id varchar(64),
@@ -18,8 +24,8 @@ create table case_worker_profile(
 );
 
 create table case_worker_location(
-	case_worker_location_id bigint,
-	case_worker_id varchar(64),
+	case_worker_location_id serial,
+	case_worker_id varchar(64) not null,
 	location varchar(128),
     location_id int not null,
     primary_flag boolean,
@@ -32,7 +38,7 @@ create table case_worker_location(
 
 
 create table case_worker_work_area(
-	case_worker_work_area_id bigint,
+	case_worker_work_area_id serial,
 	case_worker_id varchar(64) not null,
 	area_of_work varchar(128) not null,
     service_code varchar(16) not null,
@@ -43,7 +49,7 @@ create table case_worker_work_area(
 );
 
 create table case_worker_role(
-	case_worker_role_id bigint,
+	case_worker_role_id serial,
 	case_worker_id varchar(64) not null,
 	role_id int not null,
 	primary_flag boolean,
@@ -70,7 +76,7 @@ create table user_type(
 );
 
 create table case_worker_idam_role_assoc(
-    cw_idam_role_assoc_id bigint,
+    cw_idam_role_assoc_id serial,
     role_id int not null,
 	service_code varchar(16) not null,
 	idam_role varchar(64) not null,
@@ -80,7 +86,7 @@ create table case_worker_idam_role_assoc(
 );
 
 create table case_worker_audit (
-    job_id bigint,
+    job_id serial,
     authenticated_user_id varchar(32),
     job_start_time timestamp not null,
     file_name varchar(64) not null,
@@ -91,10 +97,10 @@ create table case_worker_audit (
 );
 
 create table case_worker_exception (
-   id bigint,
+   id bigint  default EXCEPTION_ID_SEQ.nextval,
    job_id bigint not null,
    excel_row_id varchar(32),
-   email_id varchar(32),
+   key varchar(256),
    field_in_error varchar(256),
    error_description varchar(512),
    updated_timestamp timestamp,
--- src/integrationTest/resources/db/testmigration/V1_4__rename_deleteFlag_to_suspended.sql
+++
@@ -1,2 +0,0 @@
--- RDCC-2079: rename DeleteFlag to Suspended
-ALTER TABLE case_worker_profile RENAME COLUMN delete_flag TO suspended;
