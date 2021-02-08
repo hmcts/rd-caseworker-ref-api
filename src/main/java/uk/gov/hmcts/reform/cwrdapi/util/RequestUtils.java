e56afd54d94fa6412221e5da1d30daf6964e3bf2
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2021-02-08 00:53:55 +0000

    RDCC-2371 - trim flow- updated functional and junits

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/util/RequestUtils.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/util/RequestUtils.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.cwrdapi.util;
 
-import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import uk.gov.hmcts.reform.cwrdapi.controllers.request.CaseWorkersProfileCreationRequest;
 
 import java.util.List;
@@ -15,12 +15,16 @@ public class RequestUtils {
     private RequestUtils() {
     }
 
+    /**
+     * trims idam roles for all requests.
+     * @param requests cw requests
+     */
     public static void trimIdamRoles(List<CaseWorkersProfileCreationRequest> requests) {
         requests.forEach(request -> {
             Set<String> roles = request.getIdamRoles();
             if (isNotEmpty(roles)) {
                 request.setIdamRoles(roles.stream()
-                        .filter(ObjectUtils::isNotEmpty)
+                        .filter(StringUtils::isNotBlank)
                         .map(String::trim)
                         .collect(toSet()));
             }
