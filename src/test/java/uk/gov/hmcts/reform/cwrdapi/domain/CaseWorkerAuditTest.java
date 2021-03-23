8ad81ef282ee200c43e46cd62010e11b3b8056cb
AdilOoze <adil.oozeerally@hmcts.net>
2021-03-23 16:09:36 +0000

    RDCC-2326: adding tests for CaseWorkerAudit

--- src/test/java/uk/gov/hmcts/reform/cwrdapi/domain/CaseWorkerAuditTest.java
+++ src/test/java/uk/gov/hmcts/reform/cwrdapi/domain/CaseWorkerAuditTest.java
@@ -3,12 +3,13 @@ package uk.gov.hmcts.reform.cwrdapi.domain;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
 
+import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class CaseWorkerAuditTest {
     @Test
@@ -40,7 +41,38 @@ public class CaseWorkerAuditTest {
         exceptionCaseWorker.setId(1L);
 
         CaseWorkerAudit caseWorkerAudit = new CaseWorkerAudit();
-        caseWorkerAudit.setExceptionCaseWorkers(Collections.singletonList(exceptionCaseWorker));
+        caseWorkerAudit.setExceptionCaseWorkers(singletonList(exceptionCaseWorker));
         assertFalse(caseWorkerAudit.getExceptionCaseWorkers().isEmpty());
+    }
+
+    @Test
+    public void testCaseWorkerAuditBuilder() {
+
+        CaseWorkerAudit caseWorkerAudit = CaseWorkerAudit.builder()
+                .jobId(1L)
+                .authenticatedUserId("AuthId")
+                .jobStartTime(LocalDateTime.now())
+                .jobEndTime(LocalDateTime.now())
+                .fileName("Test_File_Name")
+                .status("Test Status")
+                .comments("Test Comments")
+                .exceptionCaseWorkers(singletonList(new ExceptionCaseWorker()))
+                .build();
+
+        assertNotNull(caseWorkerAudit);
+        assertThat(caseWorkerAudit.getJobId(), is(1L));
+        assertThat(caseWorkerAudit.getAuthenticatedUserId(), is("AuthId"));
+        assertNotNull(caseWorkerAudit.getJobStartTime());
+        assertThat(caseWorkerAudit.getFileName(), is("Test_File_Name"));
+        assertNotNull(caseWorkerAudit.getJobEndTime());
+        assertThat(caseWorkerAudit.getStatus(), is("Test Status"));
+        assertThat(caseWorkerAudit.getComments(), is("Test Comments"));
+        assertFalse(caseWorkerAudit.getExceptionCaseWorkers().isEmpty());
+
+        String caseWorkerAuditString = CaseWorkerAudit.builder().jobId(1L).toString();
+
+        assertTrue(caseWorkerAuditString.contains("CaseWorkerAudit.CaseWorkerAuditBuilder(jobId=1"));
+
+
     }
 }
