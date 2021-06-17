8bdafa4c36376a3a5edc645601c46a256ba3d838
AdilOoze <adil.oozeerally@hmcts.net>
2021-06-17 12:53:50 +0100

    RDCC-2888: upgrading spring-test to resolve ClassNotFoundException on TestContextAnnotationUtils

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/service/impl/CaseWorkerServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/service/impl/CaseWorkerServiceImpl.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
