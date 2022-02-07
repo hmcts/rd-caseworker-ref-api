64f475c99459823ffcc5b725a81abd39b3a8c94f
arshinsalim <arshin.salim@hmcts.net>
2022-02-07 09:48:23 +0000

    Adding the Integration Tests and Sqls

---
+++ src/integrationTest/resources/db/testmigration/V1_7__insert_update_queries_role_type.sql
@@ -0,0 +1,23 @@
+-- insert queries for role_type table new business roles 3-10
+
+UPDATE role_type
+SET description = 'National Business Centre Team Leader',
+last_update =  CURRENT_TIMESTAMP
+WHERE role_id = 6;
+
+UPDATE role_type
+SET description = 'National Business Centre Listing Team',
+last_update =  CURRENT_TIMESTAMP
+WHERE role_id = 7;
+
+UPDATE role_type
+SET description = 'National Business Centre Payments Team',
+last_update =  CURRENT_TIMESTAMP
+WHERE role_id = 8;
+
+INSERT INTO
+    role_type (role_id, description, created_date)
+VALUES
+    (11, 'National Business Centre Administrator',CURRENT_TIMESTAMP),
+    (12, 'Regional Centre Team Leader',CURRENT_TIMESTAMP),
+    (13, 'Regional Centre Administrator',CURRENT_TIMESTAMP);
