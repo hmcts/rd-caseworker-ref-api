0b799a43ac197e550d57b63b6c7366d58771b334
ishratt <tunazzina.ishrat@hmcts.net>
2022-05-18 16:41:21 +0100

    RDCC-4669: updating business roles templates

--- src/integrationTest/resources/db/testmigration/V1_2__insert_queries_role_user_type.sql
+++ src/integrationTest/resources/db/testmigration/V1_2__insert_queries_role_user_type.sql
@@ -1,6 +1,6 @@
 -- insert queries for role_type table
-INSERT INTO role_type (role_id, description, created_date) VALUES (1, 'Senior Tribunal Caseworker',CURRENT_TIMESTAMP);
-INSERT INTO role_type (role_id, description, created_date) VALUES (2, 'Tribunal Caseworker',CURRENT_TIMESTAMP);
+INSERT INTO role_type (role_id, description, created_date) VALUES (1, 'Senior Legal Caseworker',CURRENT_TIMESTAMP);
+INSERT INTO role_type (role_id, description, created_date) VALUES (2, 'Legal Caseworker',CURRENT_TIMESTAMP);
 -- insert queries for user_type table
 INSERT INTO user_type (user_type_id, description, created_date) VALUES (1, 'CTSC',CURRENT_TIMESTAMP);
 INSERT INTO user_type (user_type_id, description, created_date) VALUES (2, 'Future Operations', CURRENT_TIMESTAMP);
