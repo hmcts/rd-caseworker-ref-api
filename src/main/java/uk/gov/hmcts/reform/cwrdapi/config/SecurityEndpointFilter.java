8fc6907bd6d5722cf6e0d74c09a4ccceb4109ae4
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-05-25 11:06:25 +0100

    RDCC-4680:Changes made to incorporate review comments

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SecurityEndpointFilter.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SecurityEndpointFilter.java
@@ -1,20 +1,19 @@
 package uk.gov.hmcts.reform.cwrdapi.config;
 
 import feign.FeignException;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
 import uk.gov.hmcts.reform.cwrdapi.controllers.advice.UnauthorizedException;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
-import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 
 @Configuration
@@ -29,22 +28,6 @@ public class SecurityEndpointFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        try {
-            if (StringUtils.isNotEmpty(request.getRequestURI()) && request.getRequestURI().contains("/refdata")) {
-                logger.debug("The Service Auth token length is: "
-                        + request.getHeader(SERVICE_AUTHORIZATION2).length());
-                logger.debug("The User Auth token length : "
-                        + request.getHeader(AUTHORIZATION).length());
-                logger.debug("The User Auth token contains 'Bearer '? : "
-                        + request.getHeader(AUTHORIZATION).contains("Bearer "));
-                if (logger.isDebugEnabled()) {
-                    UserInfo userInfo = idamApi.retrieveUserInfo(request.getHeader(AUTHORIZATION));
-                    logger.debug(userInfo);
-                }
-            }
-        } catch (Exception e) {
-            logger.info("Exception while processing the user or service tokens : " + e.getMessage());
-        }
 
         try {
             filterChain.doFilter(request, response);
