2bccd7ee7e48f6b40f87b17a09612a63f694e8bc
shivajilokare <shivaji.lokare@HCTS.NET>
2022-09-06 15:58:45 +0100

    added

--- src/main/resources/db/migration/V1_17__staff_ui_tables.sql
+++ src/main/resources/db/migration/V1_17__staff_ui_tables.sql
@@ -1,37 +1,37 @@
 -- Staff: Create new tables to support Staff UI
 CREATE TABLE case_worker_skill(
-case_worker_skill_id bigint,
-case_worker_id varchar(64) NOT NULL,
-skill_id int NOT NULL,
-created_date timestamp,
-last_update timestamp,
-CONSTRAINT case_worker_skill_pk PRIMARY KEY (case_worker_skill_id)
+    case_worker_skill_id bigint,
+    case_worker_id varchar(64) NOT NULL,
+    skill_id int NOT NULL,
+    created_date timestamp,
+    last_update timestamp,
+    CONSTRAINT case_worker_skill_pk PRIMARY KEY (case_worker_skill_id)
 );
 
 CREATE TABLE skill(
-skill_id int NOT NULL,
-skill_code varchar(64) NOT NULL,
-description varchar(512) NOT NULL,
-service_id varchar(64) NOT NULL,
-user_type varchar(512) NOT NULL,
-created_date timestamp,
-last_update timestamp,
-CONSTRAINT skill_id_pk PRIMARY KEY (skill_id)
+    skill_id int NOT NULL,
+    skill_code varchar(64) NOT NULL,
+    description varchar(512) NOT NULL,
+    service_id varchar(64) NOT NULL,
+    user_type varchar(512) NOT NULL,
+    created_date timestamp,
+    last_update timestamp,
+    CONSTRAINT skill_id_pk PRIMARY KEY (skill_id)
 );
 
 ALTER TABLE case_worker_profile
 ADD COLUMN user_admin boolean DEFAULT 'N';
 
 CREATE TABLE staff_audit (
-id bigint,
-authenticated_user_id varchar(32) NOT NULL,
-request_timestamp timestamp NOT NULL,
-status varchar(32),
-error_description varchar(512),
-case_worker_id varchar(64),
-operation_type varchar(32) NOT NULL,
-request_log varchar(32)
-CONSTRAINT staff_audit_pk PRIMARY KEY (id)
+    id bigint,
+    authenticated_user_id varchar(32) NOT NULL,
+    request_timestamp timestamp NOT NULL,
+    status varchar(32),
+    error_description varchar(512),
+    case_worker_id varchar(64),
+    operation_type varchar(32) NOT NULL,
+    request_log varchar(32)
+    CONSTRAINT staff_audit_pk PRIMARY KEY (id)
 );
 
 COMMIT;
