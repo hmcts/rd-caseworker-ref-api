ff25dc53cef5c91b10f246ca3e56b5994dfbbcfd
shivajilokare <shivaji.lokare@HCTS.NET>
2022-09-06 15:46:23 +0100

    added script

--- src/integrationTest/resources/db/testmigration/V1_17__staff_ui_tables.sql
+++ src/integrationTest/resources/db/testmigration/V1_17__staff_ui_tables.sql
@@ -1,2 +1,10 @@
 -- Staff: Create new tables to support Staff UI
+CREATE TABLE case_worker_skill(
+case_worker_skill_id bigint,
+case_worker_id varchar(64) NOT NULL,
+skill_id int NOT NULL,
+created_date timestamp,
+last_update timestamp,
+CONSTRAINT case_worker_skill_pk PRIMARY KEY (case_worker_skill_id)
+);
 
