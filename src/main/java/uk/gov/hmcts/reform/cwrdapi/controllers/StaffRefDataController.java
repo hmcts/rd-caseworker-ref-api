5ac29f9576422c9c69356f9592a5425d3bf90c00
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-09-12 16:13:29 +0100

    RDCC-5419:Added swagger api doc details

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/StaffRefDataController.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/StaffRefDataController.java
@@ -1,11 +1,27 @@
 package uk.gov.hmcts.reform.cwrdapi.controllers;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Authorization;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.cwrdapi.controllers.response.StaffWorkerSkillResponse;
+import uk.gov.hmcts.reform.cwrdapi.service.StaffRefDataService;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequestMapping(
         path = "/refdata/case-worker"
@@ -18,5 +34,49 @@ public class StaffRefDataController {
 
     @Value("${loggingComponentName}")
     private String loggingComponentName;
+
+
+    @Autowired
+    StaffRefDataService staffRefDataService;
+
+    @ApiOperation(
+            value = "Staff Ui API is used  to retrieve the service specific skills "
+                    ,
+            notes = "This API will be invoked by user having organisational role of staff-admin",
+            authorizations = {
+                    @Authorization(value = "ServiceAuthorization"),
+                    @Authorization(value = "Authorization")
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    code = 200,
+                    message = "Successfully retrieved list of ServiceSkills for the request provided",
+                    response = StaffWorkerSkillResponse.class
+            ),
+            @ApiResponse(
+                    code = 400,
+                    message = "Bad Request"
+            ),
+            @ApiResponse(
+                    code = 401,
+                    message = "Forbidden Error: Access denied"
+            ),
+            @ApiResponse(
+                    code = 500,
+                    message = "Internal Server Error"
+            )
+    })
+    @GetMapping(
+            produces = APPLICATION_JSON_VALUE,
+            path = {"/skill"}
+    )
+    public ResponseEntity<StaffWorkerSkillResponse> retrieveAllServiceSkills() {
+        log.info("StaffRefDataController.retrieveAllServiceSkills Calling Service layer");
+
+        StaffWorkerSkillResponse staffWorkerSkillResponse = staffRefDataService.getServiceSkills();
+
+        return ResponseEntity.ok().body(staffWorkerSkillResponse);
+    }
 
 }
