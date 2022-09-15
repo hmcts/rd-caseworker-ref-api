8b8ae1075f5d4ac251fdedadf2c77f2a0d7cbc92
shivajilokare <shivaji.lokare@HCTS.NET>
2022-09-15 10:50:45 +0100

    Staff : Create a new API for updating an existing user

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/CaseWorkerRefController.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/CaseWorkerRefController.java
@@ -12,11 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.reform.cwrdapi.client.domain.ServiceRoleMapping;
 import uk.gov.hmcts.reform.cwrdapi.controllers.advice.InvalidRequestException;
@@ -140,4 +136,18 @@ public class CaseWorkerRefController {
                 .status(idamRolesMappingResponse.getStatusCode())
                 .body(idamRolesMappingResponse);
     }
+
+    @PutMapping(
+            path = "/refdata/case-worker/profile",
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE
+    )
+    @Secured("cwd-admin")
+    public ResponseEntity<Object> updateStaffUser(@RequestBody List<ServiceRoleMapping>
+                                                                serviceRoleMappings) {
+        //TODO
+        return null;
+    }
+
+
 }
