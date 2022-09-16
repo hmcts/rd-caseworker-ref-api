f3e24d8b2013e8db783a8dfa649c2566d7af3e01
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-09-16 10:31:15 +0100

    RDCC-5419:Changes made to update pact test cases

--- src/contractTest/java/uk/gov/hmcts/reform/cwrdapi/StaffReferenceDataProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/cwrdapi/StaffReferenceDataProviderTest.java
@@ -15,14 +15,18 @@ import feign.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.cwrdapi.controllers.CaseWorkerRefUsersController;
+import uk.gov.hmcts.reform.cwrdapi.controllers.StaffRefDataController;
 import uk.gov.hmcts.reform.cwrdapi.controllers.feign.LocationReferenceDataFeignClient;
 import uk.gov.hmcts.reform.cwrdapi.controllers.internal.StaffReferenceInternalController;
 import uk.gov.hmcts.reform.cwrdapi.controllers.response.LrdOrgInfoServiceResponse;
@@ -31,12 +35,15 @@ import uk.gov.hmcts.reform.cwrdapi.domain.CaseWorkerProfile;
 import uk.gov.hmcts.reform.cwrdapi.domain.CaseWorkerRole;
 import uk.gov.hmcts.reform.cwrdapi.domain.CaseWorkerWorkArea;
 import uk.gov.hmcts.reform.cwrdapi.domain.RoleType;
+import uk.gov.hmcts.reform.cwrdapi.domain.Skill;
 import uk.gov.hmcts.reform.cwrdapi.domain.UserType;
 import uk.gov.hmcts.reform.cwrdapi.repository.CaseWorkerProfileRepository;
 import uk.gov.hmcts.reform.cwrdapi.repository.CaseWorkerWorkAreaRepository;
+import uk.gov.hmcts.reform.cwrdapi.repository.SkillRepository;
 import uk.gov.hmcts.reform.cwrdapi.service.CaseWorkerServiceFacade;
 import uk.gov.hmcts.reform.cwrdapi.service.impl.CaseWorkerDeleteServiceImpl;
 import uk.gov.hmcts.reform.cwrdapi.service.impl.CaseWorkerServiceImpl;
+import uk.gov.hmcts.reform.cwrdapi.service.impl.StaffRefDataServiceImpl;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -58,18 +65,19 @@ import static org.mockito.Mockito.when;
 @PactBroker(scheme = "${PACT_BROKER_SCHEME:http}",
         host = "${PACT_BROKER_URL:localhost}", port = "${PACT_BROKER_PORT:80}", consumerVersionSelectors = {
         @VersionSelector(tag = "master")})
-@Import(CaseWorkerProviderTestConfiguration.class)
-@SpringBootTest(properties = {"crd.publisher.caseWorkerDataPerMessage=1"})
+//@Import(CaseWorkerProviderTestConfiguration.class)
+//@SpringBootTest(properties = {"crd.publisher.caseWorkerDataPerMessage=1"})
 @IgnoreNoPactsToVerify
+@ExtendWith(MockitoExtension.class)
 public class StaffReferenceDataProviderTest {
 
-    @Autowired
+    @MockBean
     private CaseWorkerServiceImpl caseWorkerServiceImpl;
 
-    @Autowired
+    @MockBean
     private CaseWorkerDeleteServiceImpl caseWorkerDeleteServiceImpl;
 
-    @Autowired
+    @MockBean
     private CaseWorkerProfileRepository caseWorkerProfileRepo;
 
     @MockBean
@@ -78,8 +86,12 @@ public class StaffReferenceDataProviderTest {
     @MockBean
     private LocationReferenceDataFeignClient locationReferenceDataFeignClient;
 
-    @Autowired
-    private DataSource ds;
+    @Mock
+    SkillRepository skillRepository;
+    @InjectMocks
+    private StaffRefDataServiceImpl staffRefDataServiceImpl;
+    //@Autowired
+    //private DataSource ds;
 
     @Mock
     private CaseWorkerServiceFacade caseWorkerServiceFacade;
@@ -98,13 +110,16 @@ public class StaffReferenceDataProviderTest {
     @BeforeEach
     void beforeCreate(PactVerificationContext context) {
         MockMvcTestTarget testTarget = new MockMvcTestTarget();
+        System.getProperties().setProperty("pact.verifier.publishResults", "true");
         testTarget.setControllers(
                 new CaseWorkerRefUsersController(
                         "RD-Caseworker-Ref-Api", 20, "caseWorkerId",
                         "preview", caseWorkerServiceImpl, caseWorkerDeleteServiceImpl),
                 new StaffReferenceInternalController(
                         "RD-Caseworker-Ref-Api", 20, "caseWorkerId",
-                        caseWorkerServiceImpl)
+                        caseWorkerServiceImpl),
+                new StaffRefDataController("RD-Caseworker-Ref-Api"
+                        ,staffRefDataServiceImpl)
         );
         if (context != null) {
             context.setTarget(testTarget);
@@ -149,6 +164,46 @@ public class StaffReferenceDataProviderTest {
         doReturn(page).when(caseWorkerProfileRepo).findByServiceCodeIn(anySet(), any());
     }
 
+    @State({"A list of staff ref data Service skills"})
+    public void fetchListOfServiceSkills() throws JsonProcessingException {
+        List<Skill> skills = getSkillsData();
+        when(skillRepository.findAll()).thenReturn(skills);
+
+    }
+
+    private  List<Skill> getSkillsData(){
+        Skill skill1 = new Skill();
+        skill1.setServiceId("BBA3");
+        skill1.setSkillId(1l);
+        skill1.setSkillCode("A1");
+        skill1.setDescription("desc1");
+        skill1.setUserType("user_type1");
+
+        Skill skill2 = new Skill();
+        skill2.setServiceId("BBA3");
+        skill2.setSkillId(3l);
+        skill2.setSkillCode("A3");
+        skill2.setDescription("desc3");
+        skill2.setUserType("user_type3");
+
+
+        Skill skill3 = new Skill();
+        skill3.setServiceId("ABA1");
+        skill3.setSkillId(2l);
+        skill3.setSkillCode("A2");
+        skill3.setDescription("desc2");
+        skill3.setUserType("user_type2");
+
+        Skill skill4 = new Skill();
+        skill4.setServiceId("ABA1");
+        skill4.setSkillId(4l);
+        skill4.setSkillCode("A4");
+        skill4.setDescription("desc4");
+        skill4.setUserType("user_type4");
+
+        List<Skill> skills = List.of(skill1,skill2,skill3,skill4);
+        return  skills;
+    }
 
     private CaseWorkerProfile getCaseWorkerProfile(String caseWorkerId) {
         LocalDateTime timeNow = LocalDateTime.now();
