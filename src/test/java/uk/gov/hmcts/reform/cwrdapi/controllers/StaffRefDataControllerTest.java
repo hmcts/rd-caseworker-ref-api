a61c74371a12baf1619453151a3c56099a995182
m_659064 <vilas.shelke@HMCTS.NET>
2022-09-21 16:30:25 +0100

    RDCC-5417 - Renamed the test class

--- src/test/java/uk/gov/hmcts/reform/cwrdapi/controllers/StaffRefDataControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/cwrdapi/controllers/StaffRefDataControllerTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class StaffRefDataUserTypeControllerTest {
+class StaffRefDataControllerTest {
 
     StaffRefDataService staffRefDataServiceMock;
     StaffRefDataUserTypesResponse srResponse;
