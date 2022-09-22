6f4fd2e7cf8961f5cd90923a973faf2b52e8e8f2
m_659064 <vilas.shelke@HMCTS.NET>
2022-09-22 16:11:00 +0100

    RDCC-5417 - Build failed fixes

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/response/StaffRefDataUserType.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/response/StaffRefDataUserType.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts.reform.cwrdapi.controllers.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
