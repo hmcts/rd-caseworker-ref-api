8c83d3fdd2e8667fb0e6a729c656ff7a98992ea4
hmcts <prudhvi.maddineni@hmcts.net>
2022-09-26 12:07:32 +0100

    Sonarcube code coverage changes

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/response/StaffRefDataJobTitle.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/response/StaffRefDataJobTitle.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.cwrdapi.controllers.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,7 +13,6 @@ import uk.gov.hmcts.reform.cwrdapi.domain.RoleType;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@EqualsAndHashCode
 public class StaffRefDataJobTitle {
 
     @JsonProperty("role_id")
