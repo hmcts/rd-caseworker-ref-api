6b1630423e9d2564441bd66099c3f96ae3375186
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-09-26 10:42:53 +0100

    RDCC-5443:Changes made to add functional test cases

--- src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefDataFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefDataFunctionalTest.java
@@ -12,10 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.cwrdapi.controllers.response.SearchStaffUserResponse;
 import uk.gov.hmcts.reform.cwrdapi.controllers.response.StaffWorkerSkillResponse;
 import uk.gov.hmcts.reform.cwrdapi.util.FeatureToggleConditionExtension;
 import uk.gov.hmcts.reform.cwrdapi.util.ToggleEnable;
 import uk.gov.hmcts.reform.lib.util.serenity5.SerenityTest;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.cwrdapi.util.FeatureToggleConditionExtension.getToggledOffMessage;
@@ -31,7 +35,8 @@ public class StaffRefDataFunctionalTest extends AuthorizationFunctionalTest {
     public static final String STAFF_REF_DATA_SKILL_URL = "/refdata/case-worker/skill";
     public static final String STAFF_REF_DATA_RD_STAFF_UI_KEY =
             "StaffRefDataController.retrieveAllServiceSkills";
-
+    public static final String SEARCH_STAFF_USER_BY_NAME_KEY = "StaffRefDataController.searchStaffUserByName";
+    public static final String CASE_WORKER_PROFILE_URL = "/refdata/case-worker/profile";
 
     @Test
     @ToggleEnable(mapKey = STAFF_REF_DATA_RD_STAFF_UI_KEY, withFeature = true)
@@ -79,6 +84,65 @@ public class StaffRefDataFunctionalTest extends AuthorizationFunctionalTest {
         response.then()
                 .assertThat()
                 .statusCode(401);
+    }
+
+    @Test
+    @ToggleEnable(mapKey = SEARCH_STAFF_USER_BY_NAME_KEY, withFeature = true)
+    @ExtendWith(FeatureToggleConditionExtension.class)
+    void should_return_staff_user_with_status_code_200_when_flag_enabled() {
+
+        String searchString = "cwr-test";
+
+        Response fetchResponse = caseWorkerApiClient
+                .getMultipleAuthHeadersWithoutContentType(ROLE_CWD_ADMIN)
+                .get(CASE_WORKER_PROFILE_URL + "/search-by-name?search=" + searchString)
+
+                .andReturn();
+        fetchResponse.then()
+                .assertThat()
+                .statusCode(200);
+
+        List<SearchStaffUserResponse> searchStaffUserResponse = Arrays.asList(
+                fetchResponse.getBody().as(SearchStaffUserResponse[].class));
+        assertThat(searchStaffUserResponse).isNotNull();
+        assertThat(searchStaffUserResponse.get(0).getFirstName()).contains(searchString);
+        assertThat(searchStaffUserResponse.get(0).getLastName()).contains(searchString);
+
+    }
+
+    @Test
+    @ToggleEnable(mapKey = SEARCH_STAFF_USER_BY_NAME_KEY, withFeature = false)
+    @ExtendWith(FeatureToggleConditionExtension.class)
+    void should_return_staff_user_with_status_code_403_when_flag_false() {
+
+        String searchString = "cwr-test";
+
+        Response fetchResponse = caseWorkerApiClient
+                .getMultipleAuthHeadersWithoutContentType(ROLE_CWD_ADMIN)
+                .get(CASE_WORKER_PROFILE_URL + "/search-by-name?search=" + searchString)
+
+                .andReturn();
+
+        assertThat(HttpStatus.FORBIDDEN.value()).isEqualTo(fetchResponse.statusCode());
+        assertThat(fetchResponse.getBody().asString()).contains(getToggledOffMessage());
+
+    }
+
+    @Test
+    @ToggleEnable(mapKey = SEARCH_STAFF_USER_BY_NAME_KEY, withFeature = true)
+    @ExtendWith(FeatureToggleConditionExtension.class)
+    void should_return_staff_user_with_status_code_401_when_un_authorized() {
+
+        String searchString = "cwr-test";
+
+
+        Response response = caseWorkerApiClient.withUnauthenticatedRequest()
+                .get(CASE_WORKER_PROFILE_URL + "/search-by-name?search=" + searchString)
+                .andReturn();
+        response.then()
+                .assertThat()
+                .statusCode(401);
+
     }
 
 }
