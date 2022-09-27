7ae0b065fa6880e01374dc2d4fdb08b61ac5f2d9
hmcts <prudhvi.maddineni@hmcts.net>
2022-09-27 14:42:36 +0100

    Functional Test changes

--- src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefJobTitleFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefJobTitleFunctionalTest.java
@@ -88,9 +88,9 @@ class StaffRefJobTitleFunctionalTest extends AuthorizationFunctionalTest {
     @Test
     @ExtendWith(FeatureToggleConditionExtension.class)
     @ToggleEnable(mapKey = mapKey, withFeature = true)
-    void should_fetchUserTypes_403_when_Api_toggled_on() {
+    void should_Not_Fetch_Job_Titles_403_Non_Staff_Admin() {
 
-        Response response = caseWorkerApiClient.getMultipleAuthHeadersInternal(ROLE_STAFF_ADMIN)
+        Response response = caseWorkerApiClient.getMultipleAuthHeadersInternal(ROLE_CWD_SYSTEM_USER)
                 .get("/refdata/case-worker/job-title")
                 .andReturn();
         assertThat(HttpStatus.FORBIDDEN.value()).isEqualTo(response.statusCode());
