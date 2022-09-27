40ba24f934f00e57fe29db6ca3eb58d015678117
hmcts <prudhvi.maddineni@hmcts.net>
2022-09-27 16:46:39 +0100

    Unit Test changes

--- src/test/java/uk/gov/hmcts/reform/cwrdapi/controllers/StaffRefDataJobTitleControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/cwrdapi/controllers/StaffRefDataJobTitleControllerTest.java
@@ -1,11 +1,11 @@
 package uk.gov.hmcts.reform.cwrdapi.controllers;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +14,10 @@ import uk.gov.hmcts.reform.cwrdapi.controllers.response.StaffRefJobTitleResponse
 import uk.gov.hmcts.reform.cwrdapi.domain.RoleType;
 import uk.gov.hmcts.reform.cwrdapi.service.StaffRefDataService;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,83 +25,100 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class StaffRefDataJobTitleControllerTest {
 
+    @Mock
     StaffRefDataService staffRefDataServiceMock;
-    StaffRefJobTitleResponse srJobTitleResponse;
-    ResponseEntity<Object> responseEntity;
+
     @InjectMocks
     private StaffRefDataController staffRefDataController;
-
-    List<RoleType> roleType = null;
-
-    @BeforeEach
-    void setUp() {
-        staffRefDataServiceMock = mock(StaffRefDataService.class);
-
-        srJobTitleResponse = StaffRefJobTitleResponse
-                .builder()
-                .jobTitles(Collections.emptyList())
-                .build();
-
-        responseEntity = new ResponseEntity<>(srJobTitleResponse, null, HttpStatus.OK);
-
-        roleType = new ArrayList<>();
-        roleType.add(new RoleType(1L, "Test"));
-        roleType.add(new RoleType(2L, "Test 2"));
-
-        srJobTitleResponse = StaffRefJobTitleResponse
-                .builder()
-                .jobTitles(Collections.emptyList())
-                .build();
-        MockitoAnnotations.openMocks(this);
-    }
 
 
     @Test
     void shouldFetchJobTitles() {
-        responseEntity = ResponseEntity.status(200).body(null);
-        when(staffRefDataServiceMock.getJobTitles())
-                .thenReturn(roleType);
+        //given
 
-        ResponseEntity<?> actual = staffRefDataController.retrieveJobTitles();
-        final StaffRefJobTitleResponse actualResponse = (StaffRefJobTitleResponse) actual.getBody();
-        assertNotNull(actual);
-        verify(staffRefDataServiceMock, times(1))
-                .getJobTitles();
-        assertEquals(responseEntity.getStatusCode(), actual.getStatusCode());
-        assertEquals((roleType.size()), actualResponse.getJobTitles().size());
-        List<StaffRefDataJobTitle> actualResultUserType = new ArrayList<>(actualResponse.getJobTitles());
-        //assert all attributes lists
-        assertTrue(verifyAllRoleTypes(actualResultUserType, roleType));
+        final long roleId1 = RandomUtils.nextLong();
+        final long roleId2 = RandomUtils.nextLong();
+        final String roleDescription1 = RandomStringUtils.randomAlphanumeric(10);
+        final String roleDescription2 = RandomStringUtils.randomAlphanumeric(10);
+
+        final List<RoleType> roleTypes = List.of(
+                new RoleType(roleId1, roleDescription1),
+                new RoleType(roleId2, roleDescription2));
+
+
+        final StaffRefDataJobTitle staffRefDataJobTitle1 = StaffRefDataJobTitle.builder()
+                .roleId(roleId1)
+                .roleDescription(roleDescription1)
+                .build();
+
+        final StaffRefDataJobTitle staffRefDataJobTitle2 = StaffRefDataJobTitle.builder()
+                .roleId(roleId2)
+                .roleDescription(roleDescription2)
+                .build();
+
+        final List<StaffRefDataJobTitle> staffRefDataJobTitles = List.of(staffRefDataJobTitle1, staffRefDataJobTitle2);
+        final StaffRefJobTitleResponse srJobTitleResponse =
+                StaffRefJobTitleResponse
+                        .builder()
+                        .jobTitles(staffRefDataJobTitles)
+                        .build();
+
+        final ResponseEntity<Object> expectedResponseEntity = new ResponseEntity<>(srJobTitleResponse, null, HttpStatus.OK);
+
+
+        //when
+        when(staffRefDataServiceMock.getJobTitles()).thenReturn(roleTypes);
+
+
+        //then
+        ResponseEntity<?> actualReponseEntity = staffRefDataController.retrieveJobTitles();
+        assertNotNull(actualReponseEntity);
+        assertEquals(expectedResponseEntity.getStatusCode(), actualReponseEntity.getStatusCode());
+
+        final StaffRefJobTitleResponse actualStaffRefJobTitleResponse = (StaffRefJobTitleResponse) actualReponseEntity.getBody();
+        assertNotNull(actualStaffRefJobTitleResponse);
+
+        final StaffRefJobTitleResponse expectedResponseEntityBody = (StaffRefJobTitleResponse) expectedResponseEntity.getBody();
+        assertNotNull(expectedResponseEntityBody);
+
+
+        final List<StaffRefDataJobTitle> expectedJobTitles = expectedResponseEntityBody.getJobTitles();
+        assertNotNull(actualStaffRefJobTitleResponse);
+
+        List<StaffRefDataJobTitle> actualJobTitles = actualStaffRefJobTitleResponse.getJobTitles();
+        assertNotNull(actualJobTitles);
+
+        assertEquals(expectedJobTitles.size(), actualJobTitles.size());
+        assertThat(expectedJobTitles).usingRecursiveComparison().isEqualTo(actualJobTitles);
+        verify(staffRefDataServiceMock).getJobTitles();
     }
 
 
     @Test
     void shouldFetchEmptyJobTitles() {
-        responseEntity = ResponseEntity.status(200).body(null);
-        roleType.clear();
-        when(staffRefDataServiceMock.getJobTitles())
-                .thenReturn(roleType);
+        //given
+        final StaffRefJobTitleResponse srJobTitleResponse = StaffRefJobTitleResponse
+                .builder()
+                .jobTitles(List.of())
+                .build();
 
-        ResponseEntity<?> actual = staffRefDataController.retrieveJobTitles();
-        final StaffRefJobTitleResponse actualResponse = (StaffRefJobTitleResponse) actual.getBody();
-        assertNotNull(actual);
-        verify(staffRefDataServiceMock, times(1))
-                .getJobTitles();
-        assertEquals(responseEntity.getStatusCode(), actual.getStatusCode());
-        assertEquals(0, actualResponse.getJobTitles().size());
-        Assertions.assertArrayEquals(actualResponse.getJobTitles().toArray(), roleType.toArray());
+        final ResponseEntity<Object> expectedResponseEntity = new ResponseEntity<>(srJobTitleResponse, null, HttpStatus.OK);
+        //when
+        when(staffRefDataServiceMock.getJobTitles()).thenReturn(List.of());
+
+        //then
+        final ResponseEntity<?> actualResponseEntity = staffRefDataController.retrieveJobTitles();
+        assertNotNull(actualResponseEntity);
+        assertEquals(expectedResponseEntity.getStatusCode(), actualResponseEntity.getStatusCode());
+
+        final StaffRefJobTitleResponse actualResponse = (StaffRefJobTitleResponse) actualResponseEntity.getBody();
+        assertNotNull(actualResponse);
+
+        final List<StaffRefDataJobTitle> actualJobTitles = actualResponse.getJobTitles();
+
+        assertNotNull(actualJobTitles);
+        assertTrue(actualJobTitles.isEmpty());
+        verify(staffRefDataServiceMock).getJobTitles();
     }
 
-    private boolean verifyAllRoleTypes(List<StaffRefDataJobTitle> actualResultUserType, List<RoleType> roleTypes) {
-        for (int i = 0; i < actualResultUserType.size(); i++) {
-            StaffRefDataJobTitle staffRefDataJobTitle = actualResultUserType.get(i);
-            Optional<RoleType> roleType = roleTypes.stream().filter(e ->
-                    e.getRoleId().equals(staffRefDataJobTitle.getRoleId())
-                            && e.getDescription().equals(staffRefDataJobTitle.getRoleDescription())).findAny();
-            if (!roleType.isPresent()) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
