7deb8b7de571e52e4fb81fb49b26fb1ee0a3a18b
nayeemshaik-hmcts <nayeem.shaik@hmcts.net>
2022-09-28 18:39:39 +0100

    fixed the sonar issues

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/service/impl/StaffProfileServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/service/impl/StaffProfileServiceImpl.java
@@ -4,7 +4,6 @@ import feign.Response;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
