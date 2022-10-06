fe8bcd4a39770b23b7e5f9b0635ba95b2208473a
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-10-06 17:19:37 +0100

    RDCC-5443:Changes made to incorporate review comments

--- src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefDataFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefDataFunctionalTest.java
@@ -12,15 +12,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.cwrdapi.controllers.request.CaseWorkerRoleRequest;
+import uk.gov.hmcts.reform.cwrdapi.controllers.request.CaseWorkersProfileCreationRequest;
 import uk.gov.hmcts.reform.cwrdapi.controllers.response.SearchStaffUserResponse;
 import uk.gov.hmcts.reform.cwrdapi.controllers.response.StaffWorkerSkillResponse;
 import uk.gov.hmcts.reform.cwrdapi.util.FeatureToggleConditionExtension;
 import uk.gov.hmcts.reform.cwrdapi.util.ToggleEnable;
 import uk.gov.hmcts.reform.lib.util.serenity5.SerenityTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.String.format;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.cwrdapi.util.FeatureToggleConditionExtension.getToggledOffMessage;
 
@@ -89,12 +94,25 @@ public class StaffRefDataFunctionalTest extends AuthorizationFunctionalTest {
     @Test
     @ToggleEnable(mapKey = SEARCH_STAFF_USER_BY_NAME_KEY, withFeature = true)
     @ExtendWith(FeatureToggleConditionExtension.class)
-    void should_return_staff_user_with_status_code_200_when_flag_enabled() {
+    void should_return_staff_user_with_status_code_200_when_flag_enabled_default_pagination() {
 
-        String searchString = "cwr";
+        String emailPattern = "sbnTest1234";
+        String email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+
+        createCaseWorkerTestData("sbn-James", "sbn-Smith",email);
+        email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+        createCaseWorkerTestData("sbn-Michael", "sbn-Smith",email);
+        email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+        createCaseWorkerTestData("sbn-Maria", "sbn-Garcia",email);
+        email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+        createCaseWorkerTestData("sbn-Ron", "sbn-David",email);
+        email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+        createCaseWorkerTestData("sbn-Mary", "sbn-David",email);
+
+        String searchString = "sbn";
 
         Response fetchResponse = caseWorkerApiClient
-                .getMultipleAuthHeadersWithoutContentType(ROLE_CWD_ADMIN)
+                .getMultipleAuthHeadersWithoutContentType(ROLE_STAFF_ADMIN)
                 .get(CASE_WORKER_PROFILE_URL + "/search-by-name?search=" + searchString)
 
                 .andReturn();
@@ -102,11 +120,61 @@ public class StaffRefDataFunctionalTest extends AuthorizationFunctionalTest {
                 .assertThat()
                 .statusCode(200);
 
+
+        int totalRecords = Integer.valueOf(fetchResponse.getHeader("total-records"));
+
+        assertThat(totalRecords).isGreaterThan(0);
+
         List<SearchStaffUserResponse> searchStaffUserResponse = Arrays.asList(
                 fetchResponse.getBody().as(SearchStaffUserResponse[].class));
         assertThat(searchStaffUserResponse).isNotNull();
         assertThat(searchStaffUserResponse.get(0).getFirstName()).contains(searchString);
         assertThat(searchStaffUserResponse.get(0).getLastName()).contains(searchString);
+        assertThat(searchStaffUserResponse.get(0).getFirstName()).contains("sbn-Mary");
+        assertThat(searchStaffUserResponse.get(0).getLastName()).contains("sbn-David");
+    }
+
+    @Test
+    @ToggleEnable(mapKey = SEARCH_STAFF_USER_BY_NAME_KEY, withFeature = true)
+    @ExtendWith(FeatureToggleConditionExtension.class)
+    void should_return_staff_user_with_status_code_200_when_flag_enabled_with_pagination() {
+
+        String emailPattern = "sbnTest1234";
+        String email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+
+        createCaseWorkerTestData("sbn-James", "sbn-Smith",email);
+        email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+        createCaseWorkerTestData("sbn-Michael", "sbn-Smith",email);
+        email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+        createCaseWorkerTestData("sbn-Maria", "sbn-Garcia",email);
+        email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+        createCaseWorkerTestData("sbn-Ron", "sbn-David",email);
+        email = format(EMAIL_TEMPLATE, randomAlphanumeric(10) + emailPattern).toLowerCase();
+        createCaseWorkerTestData("sbn-Mary", "sbn-David",email);
+
+        String searchString = "sbn";
+        Response fetchResponse = caseWorkerApiClient
+                .getMultipleAuthHeadersWithoutContentTypeWithPagination(ROLE_STAFF_ADMIN,"1","1")
+                .get(CASE_WORKER_PROFILE_URL + "/search-by-name?search=" + searchString)
+
+                .andReturn();
+        fetchResponse.then()
+                .assertThat()
+                .statusCode(200);
+
+
+        int totalRecords = Integer.valueOf(fetchResponse.getHeader("total-records"));
+
+        assertThat(totalRecords).isGreaterThan(0);
+
+        List<SearchStaffUserResponse> searchStaffUserResponse = Arrays.asList(
+                fetchResponse.getBody().as(SearchStaffUserResponse[].class));
+        assertThat(searchStaffUserResponse).isNotNull();
+        assertThat(searchStaffUserResponse).hasSize(1);
+        assertThat(searchStaffUserResponse.get(0).getFirstName()).contains(searchString);
+        assertThat(searchStaffUserResponse.get(0).getLastName()).contains(searchString);
+        assertThat(searchStaffUserResponse.get(0).getFirstName()).contains("sbn-Mary");
+        assertThat(searchStaffUserResponse.get(0).getLastName()).contains("sbn-David");
 
     }
 
@@ -118,7 +186,7 @@ public class StaffRefDataFunctionalTest extends AuthorizationFunctionalTest {
         String searchString = "cwr";
 
         Response fetchResponse = caseWorkerApiClient
-                .getMultipleAuthHeadersWithoutContentType(ROLE_CWD_ADMIN)
+                .getMultipleAuthHeadersWithoutContentType(ROLE_STAFF_ADMIN)
                 .get(CASE_WORKER_PROFILE_URL + "/search-by-name?search=" + searchString)
 
                 .andReturn();
@@ -158,5 +226,18 @@ public class StaffRefDataFunctionalTest extends AuthorizationFunctionalTest {
                 .assertThat()
                 .statusCode(403);
     }
+
+    static void createCaseWorkerTestData(String firstName, String lastName, String email) {
+        List<CaseWorkerRoleRequest> roleRequests = new ArrayList<CaseWorkerRoleRequest>();
+        roleRequests.add(new CaseWorkerRoleRequest("National Business Centre Team Leader", true));
+        roleRequests.add(new CaseWorkerRoleRequest("Regional Centre Team Leader", false));
+        List<CaseWorkersProfileCreationRequest> caseWorkersProfileCreationRequests = caseWorkerApiClient
+                .createCaseWorkerProfiles(firstName, lastName, email);
+        caseWorkersProfileCreationRequests.get(0).setRoles(roleRequests);
+        Response response = caseWorkerApiClient.createUserProfiles(caseWorkersProfileCreationRequests);
+
+
+    }
+
 
 }
