ec751149fbc90233ffc80f9b4125d7c5b6236ba8
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-10-11 19:53:27 +0100

    RDCC-5457:Changes made to add unit test cases

--- src/test/java/uk/gov/hmcts/reform/cwrdapi/repository/CaseWorkerProfileRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/cwrdapi/repository/CaseWorkerProfileRepositoryTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.cwrdapi.domain.CaseWorkerProfile;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -36,5 +38,21 @@ class CaseWorkerProfileRepositoryTest {
         when(cwProfileRepository.findByCaseWorkerId(any())).thenReturn(Optional.of(new CaseWorkerProfile()));
         assertNotNull(cwProfileRepository.findByCaseWorkerId(any()));
         verify(cwProfileRepository, times(1)).findByCaseWorkerId(any());
+    }
+
+    @Test
+    void findByCaseWorkerIds() {
+        List<CaseWorkerProfile> caseworkerProfiles = Collections.singletonList(new CaseWorkerProfile());
+        List<String> Ids = Collections.singletonList("id_abcd");
+        when(cwProfileRepository.findByCaseWorkerIdIn(Ids)).thenReturn(caseworkerProfiles);
+        assertNotNull(cwProfileRepository.findByCaseWorkerIdIn(Ids));
+        verify(cwProfileRepository, times(1)).findByCaseWorkerIdIn(any());
+    }
+
+    @Test
+    void findByEmailId() {
+        when(cwProfileRepository.findByEmailId(any())).thenReturn(new CaseWorkerProfile());
+        assertNotNull(cwProfileRepository.findByEmailId(any()));
+        verify(cwProfileRepository, times(1)).findByEmailId(any());
     }
 }
