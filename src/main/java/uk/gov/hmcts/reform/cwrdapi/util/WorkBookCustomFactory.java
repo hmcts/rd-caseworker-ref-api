001f57d511e5a0663f747e404e09c1e5d3134cbc
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-11-28 17:59:31 +0000

    RDCC-CVE_26336:Changes made to fix cve_26336

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/util/WorkBookCustomFactory.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/util/WorkBookCustomFactory.java
@@ -1,8 +1,5 @@
 package uk.gov.hmcts.reform.cwrdapi.util;
 
-import org.apache.poi.poifs.filesystem.DirectoryNode;
-import org.apache.poi.poifs.filesystem.FileMagic;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,13 +9,10 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-import static uk.gov.hmcts.reform.cwrdapi.util.CaseWorkerConstants.CLASS_HSSF_WORKBOOK_FACTORY;
-import static uk.gov.hmcts.reform.cwrdapi.util.CaseWorkerConstants.CLASS_XSSF_WORKBOOK_FACTORY;
-import static uk.gov.hmcts.reform.cwrdapi.util.CaseWorkerConstants.ERROR_PARSING_EXCEL_FILE_ERROR_MESSAGE;
 import static uk.gov.hmcts.reform.cwrdapi.util.CaseWorkerConstants.INVALID_EXCEL_FILE_ERROR_MESSAGE;
 
-public class WorkBookCustomFactory extends WorkbookFactory {
+
+public interface WorkBookCustomFactory  {
 
     /**
      * Returns Workbook object.
@@ -30,51 +24,16 @@ public class WorkBookCustomFactory extends WorkbookFactory {
      * @throws IOException while file parsing failed
      */
     public static Workbook validateAndGetWorkBook(MultipartFile file) throws IOException {
-        InputStream is = FileMagic.prepareToCheckMagic(file.getInputStream());
-        FileMagic fm = FileMagic.valueOf(is);
-        switch (fm) {
-            case OLE2:
-                return createWorkBookForOldXls(is);
-            case OOXML:
-                return createWorkBookForNewXlsx(is);
-            default:
-                throw new ExcelValidationException(BAD_REQUEST, INVALID_EXCEL_FILE_ERROR_MESSAGE);
-        }
-    }
 
-    private static Workbook createWorkBookForOldXls(InputStream is) throws IOException {
-        //Let's use the streams to return the workbook.
-        //Converting the Multipart file to normal file will require additional permissions on server
-        //Therefore 2 different methods are required to handle the file based on type.
-        POIFSFileSystem fs = new POIFSFileSystem(is);
-        DirectoryNode directoryNode = fs.getRoot();
-        initHssf();
-        return createHssfByNode.apply(directoryNode);
-    }
-
-    private static Workbook createWorkBookForNewXlsx(InputStream is) throws IOException {
-        initXssf();
-        return createXssfByStream.apply(is);
-    }
-
-
-    private static void initXssf() {
-        if (createXssfFromScratch == null) {
-            initFactory(CLASS_XSSF_WORKBOOK_FACTORY);
-        }
-    }
-
-    private static void initHssf() {
-        if (createHssfFromScratch == null) {
-            initFactory(CLASS_HSSF_WORKBOOK_FACTORY);
-        }
-    }
-
-    private static void initFactory(String factoryClass) {
+        InputStream initialStream = file.getInputStream();
+        Workbook workbook = null;
         try {
-            Class.forName(factoryClass, true, WorkbookFactory.class.getClassLoader());
-        } catch (ClassNotFoundException e) {
-            throw new ExcelValidationException(INTERNAL_SERVER_ERROR, ERROR_PARSING_EXCEL_FILE_ERROR_MESSAGE);
+            workbook = WorkbookFactory.create(initialStream);
+            return workbook;
+        } catch (IOException exception) {
+            throw new ExcelValidationException(BAD_REQUEST, INVALID_EXCEL_FILE_ERROR_MESSAGE);
         }
+
     }
+
 }
