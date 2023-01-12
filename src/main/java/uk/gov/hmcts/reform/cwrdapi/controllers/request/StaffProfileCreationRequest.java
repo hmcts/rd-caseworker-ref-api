c76555fb2a97729d727d3a45dd60680dd41676b4
ishratt <tunazzina.ishrat@hmcts.net>
2023-01-12 15:45:17 +0000

    fix checkstyle issue

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/request/StaffProfileCreationRequest.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/controllers/request/StaffProfileCreationRequest.java
@@ -14,7 +14,6 @@ import uk.gov.hmcts.reform.cwrdapi.util.ValidateEmail;
 import uk.gov.hmcts.reform.cwrdapi.util.ValidateStaffProfileChildren;
 
 import java.util.List;
-import java.util.Set;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
