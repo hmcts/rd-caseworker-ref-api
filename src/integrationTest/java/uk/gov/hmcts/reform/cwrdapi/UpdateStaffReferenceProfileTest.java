6c46c843449cfc9228d0bd42edcec029c31e8b0f
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-02-14 13:26:50 +0000

    Integration test cases

--- src/integrationTest/java/uk/gov/hmcts/reform/cwrdapi/UpdateStaffReferenceProfileTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/cwrdapi/UpdateStaffReferenceProfileTest.java
@@ -448,15 +448,15 @@ public class UpdateStaffReferenceProfileTest extends AuthorizationEnabledIntegra
         userProfilePostUserWireMockForStaffProfile(false);
         userProfilePostUserWireMockForStaffProfile(true);
 
-        request.setResendInvite(true);
-        Map<String, Object> resendResponse = caseworkerReferenceDataClient.updateStaffProfile(request,ROLE_STAFF_ADMIN);
-
         Map<String, Object> createResponse = caseworkerReferenceDataClient.createStaffProfile(request,ROLE_STAFF_ADMIN);
+        request.setResendInvite(true);
         Map createBody = (Map)createResponse.get("body");
-        Map resendResponseBody = (Map) resendResponse.get("body");
+        Map<String, Object> resendResponse = caseworkerReferenceDataClient.updateStaffProfile(request,ROLE_STAFF_ADMIN);
         assertThat(resendResponse).isNotNull();
         assertThat(resendResponse.get("http_status")).isEqualTo("200 OK");
+        Map resendResponseBody = (Map) resendResponse.get("body");
         assertNotEquals(createBody.get("case_worker_id"), resendResponseBody.get("case_worker_id"));
+
         String path = "/profile/search-by-name";
         ResponseEntity<SearchStaffUserResponse[]> fetchStaff = caseworkerReferenceDataClient
                 .searchStaffUserByNameExchange(path, request.getFirstName(), "1", "1",
