33614a6a1f38882e392f9f35d04658da7a7b7328
shivajihmcts <89258035+shivajihmcts@users.noreply.github.com>
2023-03-06 10:46:49 +0000

    Feat:Staff : Filter the list of Skills based on Service Code (#663)
    * RDCC-6343:Changes made to add parameter for service code
    
    
    * RDCC-6343:Changes made to fix chart upgrade version to 13
    
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/repository/SkillRepository.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/repository/SkillRepository.java
@@ -1,12 +1,21 @@
 package uk.gov.hmcts.reform.cwrdapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.cwrdapi.domain.Skill;
 
-@Repository
-public interface SkillRepository extends JpaRepository<Skill,Long> {
+import java.util.List;
 
+@Repository
+public interface SkillRepository extends JpaRepository<Skill, Long> {
+
+
+    @Query(value = """
+            SELECT staffSkill FROM skill staffSkill
+            WHERE staffSkill.serviceId IN (:serviceCodes)
+            """)
+    List<Skill> getSkillsByServiceCodes(List<String> serviceCodes);
 
 }
 
