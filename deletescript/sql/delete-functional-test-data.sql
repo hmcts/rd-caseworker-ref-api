4ac203864bfc97fe0cc64dc6d4399015e0876ed9
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-05-10 10:14:26 +0100

    [RDCC-6200]-removed one extara email format

--- deletescript/sql/delete-functional-test-data.sql
+++ deletescript/sql/delete-functional-test-data.sql
@@ -1,6 +1,5 @@
 delete from case_worker_profile cwp where
       email_id like any (values('cwr-rd-func-test-user-only%'))
-   or email_id like any (values('cwr-func-test-user-%'))
    or email_id like any(values('staff-rd-profile-func-test-user-only-%')) ;
 
 commit;
