4132777d38dd33f3fa54f874221a8b2634960b45
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-05-17 13:52:05 +0100

    [RDCC-6750]Staff UI : Add new Skills for Family Private Law

--- src/main/resources/db/migration/V1_22__update_skill_table.sql
+++ src/main/resources/db/migration/V1_22__update_skill_table.sql
@@ -1,3 +1,4 @@
+
 INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
 	 (39,'SKILL:ABA5:GATEKEEPINGC100','Gatekeeping C100','ABA5','Legal Office',timezone('utc', now()),timezone('utc', now()));
 
