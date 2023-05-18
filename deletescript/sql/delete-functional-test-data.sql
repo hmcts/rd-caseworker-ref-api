4f54c901f16c6a9d3e3ef2e4e50634c921e36d5b
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-05-18 14:49:35 +0100

    [RDCC-6200] Sql query to delete functional test users from diff tables

--- deletescript/sql/delete-functional-test-data.sql
+++ deletescript/sql/delete-functional-test-data.sql
@@ -1,5 +1,43 @@
+
+delete from  case_worker_location
+where case_worker_id in (
+ select cwp.case_worker_id from case_worker_profile  cwp
+ where cwp.email_id  like (values('cwr-rd-func-test-user-only-%'))
+ );
+
+delete from  case_worker_work_area
+where case_worker_id in (
+ select cwp.case_worker_id from case_worker_profile  cwp
+ where cwp.email_id  like (values('cwr-rd-func-test-user-only-%'))
+ );
+
+delete from  case_worker_role
+where case_worker_id in (
+ select cwp.case_worker_id from case_worker_profile  cwp
+ where cwp.email_id  like (values('cwr-rd-func-test-user-only-%'))
+);
+
+
+delete from  case_worker_location
+where case_worker_id in (
+ select cwp.case_worker_id from case_worker_profile  cwp
+ where cwp.email_id  like (values('staff-rd-profile-func-test-user-only-%'))
+ );
+
+delete from  case_worker_work_area
+where case_worker_id in (
+ select cwp.case_worker_id from case_worker_profile  cwp
+ where cwp.email_id  like (values('staff-rd-profile-func-test-user-only-%'))
+ );
+
+delete from  case_worker_role
+where case_worker_id in (
+ select cwp.case_worker_id from case_worker_profile  cwp
+ where cwp.email_id  like (values('staff-rd-profile-func-test-user-only-%'))
+);
+
 delete from case_worker_profile cwp where
-      email_id like any (values('cwr-rd-func-test-user-only%'))
-   or email_id like any(values('staff-rd-profile-func-test-user-only-%')) ;
+      email_id like (values('cwr-rd-func-test-user-only-%'))
+   or email_id like (values('staff-rd-profile-func-test-user-only-%')) ;
 
 commit;
