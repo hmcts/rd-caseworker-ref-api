e6c1dfb30fe2693d2ad20be34959e4f00f729522
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2023-07-07 15:37:02 +0100

    DTSRD-471: Update skill table with the new skills for ET (#745)
    * DTSRD-471: Update skill table with the new skills for ET
    
    * DTSRD-471: Update skill table with the new skills for ET
    
    * Fix CVE-2023-2976 issue
    
    * Fix CVE-2023-2976 issue (launchdarkly-java-server-sdk upgrade to 6.2.1)
    
    * Fix CVE-2023-2976 issue (launchdarkly-java-server-sdk upgrade to 6.2.1)

--- src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefDataSkillsFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefDataSkillsFunctionalTest.java
@@ -54,8 +54,8 @@ public class StaffRefDataSkillsFunctionalTest extends AuthorizationFunctionalTes
         assertThat(staffWorkerSkillResponse.getServiceSkills().size()).isGreaterThan(0);
 
         ServiceSkill serviceSkill = staffWorkerSkillResponse.getServiceSkills().get(0);
-        assertThat(serviceSkill.getId()).isEqualTo("ABA5");
-        assertThat(serviceSkill.getSkills().size()).isGreaterThan(1);
+        assertThat(serviceSkill.getId()).isEqualTo("BHA1");
+        assertThat(serviceSkill.getSkills().size()).isGreaterThanOrEqualTo(1);
 
     }
 
---
+++ src/main/resources/db/migration/V1_23__update_skill_table.sql
@@ -0,0 +1,2 @@
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (41,'SKILL:BHA1:ET1Vetting','ET1 Vetting','BHA1','CTSC',timezone('utc', now()),timezone('utc', now()));
