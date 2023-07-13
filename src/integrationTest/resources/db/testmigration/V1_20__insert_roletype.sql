bc88572537d4472c999dd2042aaf9f9f1fa7150c
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-07-13 10:55:07 +0100

    DTSRD-742, DTSRD-541: add new roles

---
+++ src/integrationTest/resources/db/testmigration/V1_20__insert_roletype.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO
+role_type (role_id, description, created_date)
+VALUES
+(17, 'CICA Caseworker', timezone('utc', now()));
+
+INSERT INTO
+role_type (role_id, description, created_date)
+VALUES
+(18, 'Cafcass Cymru Caseworker', timezone('utc', now()));
