e1644c22d66655c27089e0ecd7844003b93a6b3b
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-10-17 13:50:09 +0100

    DTSRD-1248.Reverting changes for V15 made to perfdata

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,12 +1,6 @@
 provider "azurerm" {
   features {}
 }
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-  alias                      = "postgres_network"
-  subscription_id            = var.aks_subscription_id
-}
 terraform {
   backend "azurerm" {}
 
