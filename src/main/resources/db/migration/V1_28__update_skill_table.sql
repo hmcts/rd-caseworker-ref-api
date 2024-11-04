23dd99c1f36b26bcb956fbc3d336310ff6666e86
TomForniCGI <Tom.Forni@HMCTS.NET>
2024-11-04 16:34:41 +0000

    Initial Commit

---
+++ src/main/resources/db/migration/V1_28__update_skill_table.sql
@@ -0,0 +1,12 @@
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (42,'SKILL:AAA7:DAMAGES_CCMC_CMC_MANAGEMENT','CCMC/CMC Management','AAA7','hearing-centre-admin',timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (43,'SKILL:AAA6:MONEY_CLAIMS_CCMC_CMC_MANAGEMENT','CCMC/CMC Management','AAA6','hearing-centre-admin',timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (44,'SKILL:AAA7:DAMAGES_MULTI_TRACK_TRIAL_MANAGEMENT','Multi Track Trial Management','AAA7','hearing-centre-admin',timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (45,'SKILL:AAA6:SPECIFIED_MULTI_TRACK_TRIAL_MANAGEMENT','Multi Track Trial Management','AAA6','hearing-centre-admin',timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (46,'SKILL:AAA7:DAMAGES_INTERMEDIATE_TRACK_TRIAL_MANAGEMENT','Intermediate Track Trial Management','AAA7','hearing-centre-admin',timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (47,'SKILL:AAA6:SPECIFIED_INTERMEDIATE_TRACK_TRIAL_MANAGEMENT','Intermediate Track Trial Management','AAA6','hearing-centre-admin',timezone('utc', now()),timezone('utc', now()));
