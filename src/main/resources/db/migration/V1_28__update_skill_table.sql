828550929178f617f4b18a124af0941ab3b6dcda
Ben James <133333764+ben-james-hmcts@users.noreply.github.com>
2024-11-14 16:10:37 +0000

    Adjusted descriptions for two of the skills to match Jira description

--- src/main/resources/db/migration/V1_28__update_skill_table.sql
+++ src/main/resources/db/migration/V1_28__update_skill_table.sql
@@ -3,6 +3,6 @@ INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_d
 INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
 	 (43,'SKILL:AAA6:MONEY_CLAIMS_CCMC_CMC_MANAGEMENT','CCMC/CMC Management','AAA6','Future Operations',timezone('utc', now()),timezone('utc', now()));
 INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
-	 (44,'SKILL:AAA7:DAMAGES_MULTI_TRACK_TRIAL_MANAGEMENT','Multi Track Trial Management','AAA7','Future Operations',timezone('utc', now()),timezone('utc', now()));
+	 (44,'SKILL:AAA7:DAMAGES_MULTI_TRACK_TRIAL_MANAGEMENT','Multi and Intermediate Track Trial Management','AAA7','Future Operations',timezone('utc', now()),timezone('utc', now()));
 INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
-	 (45,'SKILL:AAA6:SPECIFIED_MULTI_TRACK_TRIAL_MANAGEMENT','Multi Track Trial Management','AAA6','Future Operations',timezone('utc', now()),timezone('utc', now()));
+	 (45,'SKILL:AAA6:SPECIFIED_MULTI_TRACK_TRIAL_MANAGEMENT','Multi and Intermediate Track Trial Management','AAA6','Future Operations',timezone('utc', now()),timezone('utc', now()));
