f0999aae164a66d859df1cb02f8d6783c70dbe03
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-12-04 20:54:54 +0000

    fix swagger documentation

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SwaggerConfiguration.java
@@ -1,5 +1,11 @@
 package uk.gov.hmcts.reform.cwrdapi.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +13,42 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfiguration {
+
+    private static final String DESCRIPTION = "API will help to provide Case worker user profile data to clients.";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components()
+                .addSecuritySchemes(
+                    "Authorization",
+                    new io.swagger.v3.oas.models.security.SecurityScheme()
+                        .name("Authorization")
+                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .description("Valid IDAM user token, (Bearer keyword is "
+                            + "added automatically)")
+                )
+                .addSecuritySchemes("ServiceAuthorization",
+                    new io.swagger.v3.oas.models.security.SecurityScheme()
+                        .in(io.swagger.v3.oas.models.security.SecurityScheme.In.HEADER)
+                        .name("ServiceAuthorization")
+                        .type(SecurityScheme.Type.APIKEY)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                        .description("Valid Service-to-Service JWT token for a "
+                            + "whitelisted micro-service")
+                )
+            )
+            .info(new Info().title("RD Case Worker Ref Api service")
+                .description(DESCRIPTION))
+            .externalDocs(new ExternalDocumentation()
+                .description("README")
+                .url("https://github.com/hmcts/rd-caseworker-ref-api/blob/master/README.md"))
+            .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+            .addSecurityItem(new SecurityRequirement().addList("ServiceAuthorization"));
+    }
 
     @Bean
     public GroupedOpenApi publicApi() {
