3a16c05f58b2b7577dff63bae2f33add154f08c9
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2024-12-04 20:44:55 +0000

    swagger api (#989)
    * swagger api
    
    * swagger api

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SwaggerConfiguration.java
@@ -1,17 +1,11 @@
 package uk.gov.hmcts.reform.cwrdapi.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@SecurityScheme(name = "Authorization", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
-@SecurityScheme(name = "ServiceAuthorization", type = SecuritySchemeType.APIKEY,
-        in = SecuritySchemeIn.HEADER, bearerFormat = "JWT", description = "ServiceAuthorization")
 public class SwaggerConfiguration {
 
     @Bean
@@ -21,4 +15,5 @@ public class SwaggerConfiguration {
                 .pathsToMatch("/**")
                 .build();
     }
+
 }
