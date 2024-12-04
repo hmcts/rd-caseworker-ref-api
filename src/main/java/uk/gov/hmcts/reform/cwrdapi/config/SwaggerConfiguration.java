06e0120381a1a71c25377a09b3915892feeb9700
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-12-04 20:32:58 +0000

    changed content type in req to match response header

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SwaggerConfiguration.java
@@ -1,17 +1,12 @@
 package uk.gov.hmcts.reform.cwrdapi.config;
 
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.Operation;
-import io.swagger.v3.oas.models.media.StringSchema;
-import io.swagger.v3.oas.models.parameters.Parameter;
 import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.HandlerMethod;
+
 
 @Configuration
 @SecurityScheme(name = "Authorization", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
