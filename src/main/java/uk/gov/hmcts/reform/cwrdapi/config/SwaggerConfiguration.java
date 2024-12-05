e538fc2930cb3a347d6d9de4e9953eb01b97699a
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-12-05 12:21:33 +0000

    fix swagger documentation

--- src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/cwrdapi/config/SwaggerConfiguration.java
@@ -1,20 +1,25 @@
 package uk.gov.hmcts.reform.cwrdapi.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.ExternalDocumentation;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.web.method.HandlerMethod;
 
 @Configuration
+@io.swagger.v3.oas.annotations.security.SecurityScheme(name = "Authorization", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
+@io.swagger.v3.oas.annotations.security.SecurityScheme(name = "ServiceAuthorization", type = SecuritySchemeType.APIKEY,
+    in = SecuritySchemeIn.HEADER, bearerFormat = "JWT", description = "ServiceAuthorization")
 public class SwaggerConfiguration {
 
-    private static final String DESCRIPTION = "API will help to provide Case worker user profile data to clients.";
+
+   /* private static final String DESCRIPTION = "API will help to provide Case worker user profile data to clients.";
 
     @Bean
     public OpenAPI openApi() {
@@ -48,14 +53,36 @@ public class SwaggerConfiguration {
                 .url("https://github.com/hmcts/rd-caseworker-ref-api/blob/master/README.md"))
             .addSecurityItem(new SecurityRequirement().addList("Authorization"))
             .addSecurityItem(new SecurityRequirement().addList("ServiceAuthorization"));
+    }*/
+
+    @Bean
+    public GroupedOpenApi publicApi(OperationCustomizer customGlobalHeaders) {
+        return GroupedOpenApi.builder()
+            .group("rd-caseworker-ref-api")
+            .pathsToMatch("/**")
+            .build();
     }
 
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("rd-caseworker-ref-api")
-                .pathsToMatch("/**")
-                .build();
+    public OperationCustomizer customGlobalHeaders() {
+        return (Operation customOperation, HandlerMethod handlerMethod) -> {
+            Parameter serviceAuthorizationHeader = new Parameter()
+                .in(ParameterIn.HEADER.toString())
+                .schema(new StringSchema())
+                .name("ServiceAuthorization")
+                .description("Keyword `Bearer` followed "
+                    + "by a service-to-service token for a whitelisted micro-service")
+                .required(true);
+            Parameter authorizationHeader = new Parameter()
+                .in(ParameterIn.HEADER.toString())
+                .schema(new StringSchema())
+                .name("Authorization")
+                .description("Authorization token")
+                .required(true);
+            customOperation.addParametersItem(authorizationHeader);
+            customOperation.addParametersItem(serviceAuthorizationHeader);
+            return customOperation;
+        };
     }
-
 }
+
