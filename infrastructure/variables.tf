57656a69a82339c0bb9ab57708e63420b1033af9
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2024-12-09 18:15:46 +0000

    CME-122: set default thresholds to 70% (#995)

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -153,19 +153,19 @@ variable "email_address_key" {
 }
 
 variable "cpu_threshold" {
-  default     = 5
+  default     = 70
   type        = number
   description = "Average CPU utilisation threshold"
 }
 
 variable "memory_threshold" {
-  default     = 5
+  default     = 70
   type        = number
   description = "Average memory utilisation threshold"
 }
 
 variable "storage_threshold" {
-  default     = 0.5
+  default     = 70
   type        = number
   description = "Average storage utilisation threshold"
 }
