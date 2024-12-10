a08ab698ed11b1c70ceb82325ba4600dbffbbfc4
Sammi Chong <shinyee.chong@hmcts.net>
2024-12-10 10:49:12 +0000

    Revert

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -70,9 +70,6 @@ module "db-rd-caseworker-ref-v16" {
   action_group_name           = join("-", [var.action_group_name, local.db_name, var.env])
   email_address_key           = var.email_address_key
   email_address_key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-  cpu_threshold               = var.cpu_threshold
-  memory_threshold            = var.memory_threshold
-  storage_threshold           = var.storage_threshold
 
 }
 
@@ -117,10 +114,6 @@ module "db-rd-caseworker-ref-v16-replica" {
   action_group_name              = join("-", [var.action_group_name, local.db_name, "replica", var.env])
   email_address_key              = var.email_address_key
   email_address_key_vault_id     = data.azurerm_key_vault.rd_key_vault.id
-  cpu_threshold                  = var.cpu_threshold
-  memory_threshold               = var.memory_threshold
-  storage_threshold              = var.storage_threshold
-
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
