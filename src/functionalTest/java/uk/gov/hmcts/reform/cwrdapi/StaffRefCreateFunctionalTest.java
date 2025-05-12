2e59ce7e107ec699bdca23db7190f7ebb20c2fe0
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-05-12 12:56:35 +0100

    DTSRD-4521.Extend email domain list to include IBCA users

--- src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefCreateFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/cwrdapi/StaffRefCreateFunctionalTest.java
@@ -1,5 +1,21 @@
 package uk.gov.hmcts.reform.cwrdapi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static uk.gov.hmcts.reform.cwrdapi.CaseWorkerRefFunctionalTest.DELETE_CASEWORKER_BY_ID_OR_EMAILPATTERN;
+import static uk.gov.hmcts.reform.cwrdapi.util.FeatureToggleConditionExtension.getToggledOffMessage;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
@@ -29,22 +45,6 @@ import uk.gov.hmcts.reform.cwrdapi.controllers.response.StaffProfileCreationResp
 import uk.gov.hmcts.reform.cwrdapi.util.FeatureToggleConditionExtension;
 import uk.gov.hmcts.reform.cwrdapi.util.ToggleEnable;
 import uk.gov.hmcts.reform.lib.util.serenity5.SerenityTest;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import static java.lang.String.format;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.http.HttpStatus.NO_CONTENT;
-import static org.testcontainers.shaded.org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
-import static uk.gov.hmcts.reform.cwrdapi.CaseWorkerRefFunctionalTest.DELETE_CASEWORKER_BY_ID_OR_EMAILPATTERN;
-import static uk.gov.hmcts.reform.cwrdapi.util.FeatureToggleConditionExtension.getToggledOffMessage;
 
 @ComponentScan("uk.gov.hmcts.reform.cwrdapi")
 @WithTags({@WithTag("testType:Functional")})
@@ -79,7 +79,7 @@ class StaffRefCreateFunctionalTest extends AuthorizationFunctionalTest {
 
     }
 
-@Test
+    @Test
     void createStaffProfileIncorrectEmail() {
         StaffProfileCreationRequest staffProfileCreationRequest = caseWorkerApiClient.createStaffProfile(
             "staff-rd-profile-func-test-user-only-%s@gmail.com");
