ecb6f87a09c24e49744fcbe56f3608171f218b54
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-07-09 11:55:39 +0100

    DTSRD-4521.Update JWT handling in integration tests and client

--- src/integrationTest/java/uk/gov/hmcts/reform/cwrdapi/util/JwtDecoderMockBuilder.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/cwrdapi/util/JwtDecoderMockBuilder.java
@@ -1,14 +1,18 @@
 package uk.gov.hmcts.reform.cwrdapi.util;
 
 
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtDecoderMockBuilder extends AuthorizationEnabledIntegrationTest {
+    @Autowired
+    JwtDecoder jwtDecoder;
 
-    public void resetJwtDecoder() {
+    public  void resetJwtDecoder() {
+        Mockito.reset(jwtDecoder);
     }
 
-    public synchronized JwtDecoder getJwtDecoder() {
-        return null;
-    }
 }
