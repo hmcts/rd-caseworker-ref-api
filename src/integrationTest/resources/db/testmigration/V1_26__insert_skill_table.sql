0891f54e00831d91817d2a3c0cadabdeb195b684
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-09-02 14:59:06 +0100

    DTSRD-4329. flyway script to add skills into skill table

---
+++ src/integrationTest/resources/db/testmigration/V1_26__insert_skill_table.sql
@@ -0,0 +1,45 @@
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (46,'SKILL:HRS1:AA','HRS Civil','HRS1','Future Operations',timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (47,'SKILL:HRS1:AB','HRS Family','HRS1','Future Operations',timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (48,'SKILL:HRS1:BA','HRS General Regulatory Chamber','HRS1','Future Operations',timezone('utc', now()),
+	 timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (49,'SKILL:HRS1:BB','HRS Social Entitlement Chamber','HRS1','Future Operations',timezone('utc', now()),
+	 timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (50,'SKILL:HRS1:BC','HRS Health, Education and Social Care Chamber','HRS1','Future Operations',timezone('utc',
+	 now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (51,'SKILL:HRS1:BD','HRS Tax Chamber','HRS1','Future Operations',timezone('utc', now()),timezone('utc',
+	  now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (52,'SKILL:HRS1:BE','HRS  War Pensions and Armed Forces Compensation Chamber','HRS1','Future Operations',
+	 timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (53,'SKILL:HRS1:BF','HRS Immigration and Asylum Chamber','HRS1','Future Operations',timezone('utc', now()),
+	 timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (54,'SKILL:HRS1:BG','HRS Property Chamber','HRS1','Future Operations',timezone('utc', now()),timezone('utc',
+	 now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (55,'SKILL:HRS1:BH','HRS Employment Tribunals','HRS1','Future Operations',timezone('utc', now()),timezone('utc',
+	 now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (56,'SKILL:HRS1:BT','HRS Upper Tribunal Tax and Chancery Chamber','HRS1','Future Operations',timezone('utc',
+	 now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+	 (57,'SKILL:HRS1:BL','HRS Upper Tribunal Lands Chamber','HRS1','Future Operations',timezone('utc',
+	  now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+     (58,'SKILL:HRS1:BI','HRS Upper Tribunal Immigration and Asylum Chamber','HRS1','Future Operations',
+     timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+     (59,'SKILL:HRS1:BK','HRS Upper Tribunal Administrative Appeals Chamber','HRS1','Future Operations',
+     timezone('utc', now()),timezone('utc', now()));
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+     (60,'SKILL:HRS1:BM','HRS Employment Appeals Tribunal','HRS1','Future Operations',timezone('utc',
+     now()),timezone('utc', now()));
+
+commit;
