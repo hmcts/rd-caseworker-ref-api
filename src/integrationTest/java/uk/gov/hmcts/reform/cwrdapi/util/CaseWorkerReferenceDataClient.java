6ddf15d1387ef2ff9633dc0b03b9c1697e7f5a71
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-09-03 13:58:27 +0100

    DTSRD-4329. fixing integrationtestcase

--- src/integrationTest/java/uk/gov/hmcts/reform/cwrdapi/util/CaseWorkerReferenceDataClient.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/cwrdapi/util/CaseWorkerReferenceDataClient.java
@@ -159,6 +159,9 @@ public class CaseWorkerReferenceDataClient {
 
     }
 
+
+
+
     @SuppressWarnings({"rawtypes", "unchecked"})
     private ResponseEntity<Object> getRequest(String uriPath, Class clasz, String role) {
 
