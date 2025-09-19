ac5cfb11bfb22c151e2b1304733c125128ae3400
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2025-09-19 12:34:09 +0100

    Update V1_26__delete_roletype.sql

--- src/integrationTest/resources/db/testmigration/V1_26__delete_roletype.sql
+++ src/integrationTest/resources/db/testmigration/V1_26__delete_roletype.sql
@@ -5,11 +5,12 @@ where
     role_id  ='16';
 
 delete from
+   case_worker_role cwr
+where
+   role_id ='16';
+
+delete from
     role_type
 where
     role_id = '16' and description = 'Registrar';
 
-delete from
-   case_worker_role cwr
-where
-   role_id ='16';
