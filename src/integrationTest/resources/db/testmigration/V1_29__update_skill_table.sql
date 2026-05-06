d3d483fe7206cb0478bb3d454dfbda6cfdec8b73
Anthony Dummer <anthony.dummer@HMCTS.net>
2026-05-06 17:50:38 +0100

    cot-1195 adding skills for fin rem

---
+++ src/integrationTest/resources/db/testmigration/V1_29__update_skill_table.sql
@@ -0,0 +1,10 @@
+INSERT INTO SKILL(skill_id,skill_code,description,service_id,user_type,created_date,last_update) VALUES
+    (78,'SKILL:ABA2:ManageScannedDocuments','Manage Scanned Documents','ABA2','CTSC',timezone('utc', now()),timezone('utc', now())),
+    (79,'SKILL:ABA2:CheckingHWF','Checking HWF','ABA2','CTSC',timezone('utc', now()),timezone('utc', now())),
+    (80,'SKILL:ABA2:CheckingApplications','Checking Applications','ABA2','CTSC',timezone('utc', now()),timezone('utc', now())),
+    (81,'SKILL:ABA2:ProcessApprovedOrders','Process Approved Orders','ABA2','CTSC',timezone('utc', now()),timezone('utc', now())),
+    (82,'SKILL:ABA2:CheckRefusedOrder','Check Refused Order','ABA2','CTSC',timezone('utc', now()),timezone('utc', now())),
+    (83,'SKILL:ABA2:CheckOrderResponse','Check Order Response','ABA2','CTSC',timezone('utc', now()),timezone('utc', now())),
+    (84,'SKILL:ABA2:ListforHearing','List for Hearing','ABA2','CTSC',timezone('utc', now()),timezone('utc', now())),
+    (85,'SKILL:ABA2:ClosingCases','Closing Cases','ABA2','CTSC',timezone('utc', now()),timezone('utc', now())),
+    (86,'SKILL:ABA2:ReviewingConsentApplications','Reviewing Consent Applications','ABA2','CTSC',timezone('utc', now()),timezone('utc', now()));
